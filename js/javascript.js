d1a9df2d97c4f55d4cd3474453a7ce4886cfd128
code.developer13 <dev13@codecraft.com>
2020-07-22 23:11:51 -0400

    skills added

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,13 @@
 
-
-
-
 const slideLeft = {
     distance: '150%',
     origin: 'left',
+    opacity: null,
+    duration:2000
+};
+const slideBottom = {
+    distance: '150%',
+    origin: 'bottom',
     opacity: null,
     duration:2000
 };
@@ -19,8 +22,10 @@ const slideRight = {
 const fade ={
     duration:2000
 }
+
 ScrollReveal().reveal('.slideLeft',slideLeft);
 ScrollReveal().reveal('.slideRight',slideRight);
+ScrollReveal().reveal('.slideBottom',slideBottom);
 ScrollReveal().reveal('.fadeHero',fade);
 
 
